ddd4b3cacf668b2ba94ec24b3372e7096690f972
TheMartyMan <martonstar@gmail.com>
2021-10-05 01:31:50 +0200

    3-as gyakorlat

---
+++ AJYKQ3_0929/AJYKQ3_3_8/src/ajykq3/AJYKQ3_3_8_futtat.java
@@ -0,0 +1,21 @@
+package ajykq3;
+
+
+//import  ajykq3.AJYKQ3_3_8;
+
+
+public class AJYKQ3_3_8_futtat {
+
+	public static void main(String[] args) {
+
+		/*AJYKQ3_3_8 adatok[] = AJYKQ3_3_8.beker();
+		
+		for (int i = 0; i < adatok.length; i++) {
+			System.out.println(adatok[i]);
+		}*/
+		
+		
+		
+	}
+	
+}
